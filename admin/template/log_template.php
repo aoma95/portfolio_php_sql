29934e689888896441bb69bfb4c6f5c751e17b11
Dan ESPOSITO <dan.esposito@ynov.com>
2019-07-02 14:38:04 +0200

    ajout

---
+++ admin/template/log_template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin</title>
+    <link href="/php_portfolio/admin/css/login.css" rel="stylesheet">
+  </head>
+<body>
+  <?= $log_content ?>
+</body>
